ab60b0a307131e6a1644ce67a19ec45bdf43eea3
DarkNormal <mark.lordan@me.com>
2016-07-27 09:13:55 +0100

    Initial commit

---
+++ src/MusicTest1.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Mark on 7/26/2016.
+ */
+import javax.sound.midi.*;
+public class MusicTest1 {
+    public void play(){
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            System.out.println("We got a sequencer");
+        }
+        catch (MidiUnavailableException ex){
+            System.out.println("Bummer");
+        }
+    }
+    public static void main(String[] args){
+        MusicTest1 mt = new MusicTest1();
+        mt.play();
+    }
+}
